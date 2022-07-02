7888699064b1697464086ead250e6c4f22b7af3d
Geovanne Borges <geovanneborges23@gmail.com>
2022-07-01 22:46:26 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const MenuIcon= document.getElementById('hamburger')
+const SideMenu= document.getElementById('menu-header')
+function ChangeIcon(){
+    MenuIcon.classList.toggle("change")
+    SideMenu.classList.toggle("change")
+}
